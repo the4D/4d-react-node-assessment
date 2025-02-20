7b5bc070e40432521c90d067a49baeeb1913af18
Niaz Haque <niazhaque@MacBook-Air-2.local>
2025-02-20 10:13:53 -0500

    initail commit

--- src/lib/validationRules.ts
+++ src/lib/validationRules.ts
@@ -3,5 +3,5 @@ export const validationRules = {
           required: true,
           pattern: /^[A-Za-z]{2,10}$/,
           message: 'First name must be 2-10 letters only'
-        },
+        }
 }
